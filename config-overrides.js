4299ad334e60677a2a5a4d3494777aba2e2ac676
joesch21 <joesch@hotmail.com>
2024-12-16 09:56:59 +1100

    updates

--- config-overrides.js
+++ config-overrides.js
@@ -3,10 +3,12 @@ const webpack = require("webpack");
 module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    url: require.resolve("url/"),
-    assert: require.resolve("assert/"),
-    os: require.resolve("os-browserify/browser"),
-    http: require.resolve("stream-http"),
+    url: require.resolve("url/"), // Add polyfill for 'url'
+    os: require.resolve("os-browserify/browser"), // Add polyfill for 'os'
+    http: require.resolve("stream-http"), // Add polyfill for 'http'
+    https: require.resolve("https-browserify"), // Add polyfill for 'https'
+    assert: require.resolve("assert/"), // Add polyfill for 'assert'
+    buffer: require.resolve("buffer/"), // Add polyfill for 'buffer'
   };
 
   config.plugins = [
